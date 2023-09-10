ddc31ed6c2ca7574f857de2c2a99c685ae3b42ee
M4rtinrm <rodriguezmartini71@gmail.com>
2023-09-09 19:50:50 -0500

    Updated to chapter 7

--- rango/forms.py
+++ rango/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from rango.models import Page, Category
+import re
 
-class CategoryForms(forms.ModelForm):
+class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -11,12 +12,27 @@ class CategoryForms(forms.ModelForm):
     class Meta:
         # Provide an association between the ModelForm and a model..
         model = Category
-        fields = ('name')
+        fields = ('name',)
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # Strip away any leading http or https
+        url = re.sub(r"http[s]:[\/]+", "", url)
+
+        # If url is not empty and doesn't start with 'https://',
+        # then prepend 'https://' as we want to make sure
+        # we are accessing a secure site
+        if url:
+            url = f'https://{url}'
+            cleaned_data['url'] = url
+
+        return cleaned_data
 
     class Meta:
         # Provide an association between the ModelForm and a model..
@@ -27,6 +43,8 @@ class PageForm(forms.ModelForm):
         # Some fields may allow NULL values; we may not want to include them.
         # Here, we are hiding the foreign key.
         # we can either exclude the category field from the form,,
-        exclude = ('category')
+        exclude = ('category',)
         # or specify the fields to include (don't include the category field).
         #fields = ('title', 'url', 'views').
+    
+    
